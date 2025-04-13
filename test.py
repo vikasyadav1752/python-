f889d02683c007f06762dfb3469503e83a2e5519
vikasyadav1752 <vickyyadav1752@gmail.com>
2025-04-13 22:20:41 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+
+# Question 1
+numbers = [10, 20, 30, 40, 50]
+reversed_numbers = numbers[::-1]
+print(reversed_numbers)  # [50, 40, 30, 20, 10]
+
+# Question 2 
+numbers = [10, 20, 30, 40, 50]
+index = number.index(30)
+print(index)
+
+# Question 3
+numbers = [5, 1, 5, 2, 5, 3]
+count = numbers.count(5)
+print(count)  # 3
+
+
+# Question4
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = list(set(numbers))
+print(unique_numbers)  # [1, 2, 3, 4, 5] (order may vary)
+
+#Question 5
+nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+element = nested_list[2][2]
+print(element)
+
+#Question 6
